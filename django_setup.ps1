727df64d2d97e7ba01b82984c90ab0283d9ff665
gega0397 <62949727+gega0397@users.noreply.github.com>
2024-04-04 18:30:33 +0400

    Add files via upload

---
+++ django_setup.ps1
@@ -0,0 +1,20 @@
+python -m venv venv
+
+.\venv\Scripts\Activate.ps1
+
+pip install -r requirements.txt
+
+django-admin startproject myproject
+
+cd myproject
+
+python manage.py migrate
+
+$superUserScript = @"
+from django.contrib.auth import get_user_model
+User = get_user_model()
+User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+"@
+python manage.py shell -c $superUserScript
+
+python manage.py runserver
